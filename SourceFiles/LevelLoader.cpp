1e06982aededff0821603ffa6a743fd5d4a392ac
JamesCraster <jamescraster99@gmail.com>
2016-10-01 14:15:40 +0100

    Add files via upload

--- SourceFiles/LevelLoader.cpp
+++ SourceFiles/LevelLoader.cpp
@@ -10,7 +10,7 @@
 
 
 
-std::vector<sf::RectangleShape*> loadLevelFromString(std::string levelString,int levelWidth, int levelHeight,sf::Vector2f tileSize){
+std::vector<sf::RectangleShape*> loadLevelFromString(std::string levelString,int levelWidth, int levelHeight,sf::Vector2f tileSize, std::vector<Spike*>& spikeVector){
     std::vector<std::vector<int>> levelVector (levelWidth/tileSize.x,std::vector<int> (levelHeight/tileSize.y,0));
     
     int y = 0;
@@ -28,12 +28,12 @@ std::vector<sf::RectangleShape*> loadLevelFromString(std::string levelString,int
             if(levelString[a+1] != ',' && levelString[a+1] != 'n')
             {
                 levelVector[xPos][y] = std::stoi(levelString.substr(a,2));
-                
+                xPos += 1;
+                a += 1;
                 
             }else
             {
                 levelVector[xPos][y] = std::stoi(levelString.substr(a,1));
-                
                 xPos += 1;
                 
                 
@@ -44,19 +44,38 @@ std::vector<sf::RectangleShape*> loadLevelFromString(std::string levelString,int
         
     }
     std::vector<sf::RectangleShape*> tileVector;
+    std::vector<sf::RectangleShape*> entranceVector;
     for (int x = 0; x < levelVector.size(); x++) {
         for(int y = 0; y < levelVector[x].size(); y++){
-            if(levelVector[x][y]){
+            if(levelVector[x][y] == 1){
                 tileVector.push_back(new sf::RectangleShape);
                 tileVector[tileVector.size()-1]->setPosition(x *tileSize.x, y*tileSize.y);
-                tileVector[tileVector.size()-1]->setFillColor(sf::Color(0,0,0));
+                tileVector[tileVector.size()-1]->setFillColor(sf::Color(0,0,0,0));
                 tileVector[tileVector.size()-1]->setOutlineColor(sf::Color(24,118,118));
                 tileVector[tileVector.size()-1]->setOutlineThickness(1);
-                tileVector[tileVector.size()-1]->setSize(sf::Vector2f(tileSize.x, tileSize.y));
+                tileVector[tileVector.size()-1]->setSize(sf::Vector2f(tileSize.x-1, tileSize.y-1));
+            }
+            if(levelVector[x][y] == 10){
+                spikeVector.push_back(new Spike(sf::Vector2f(x * tileSize.x,y * tileSize.y), sf::Vector2f(1,24),sf::Vector2f(tileSize.x,tileSize.x-24), sf::Vector2f(tileSize.x/2,tileSize.y/2)));
+                
+            }
+            if(levelVector[x][y] == 11){
+                spikeVector.push_back(new Spike(sf::Vector2f(x * tileSize.x,y * tileSize.y), sf::Vector2f(1,24),sf::Vector2f(tileSize.x-1,tileSize.x-24), sf::Vector2f(tileSize.x/2,tileSize.y/2)));
+                spikeVector[spikeVector.size()-1]->rotate(90);
+                
+            }
+            if(levelVector[x][y] == 12){
+                spikeVector.push_back(new Spike(sf::Vector2f(x * tileSize.x,y * tileSize.y), sf::Vector2f(1,24),sf::Vector2f(tileSize.x-1,tileSize.x-24), sf::Vector2f(tileSize.x/2,tileSize.y/2)));
+                spikeVector[spikeVector.size()-1]->rotate(180);
+                
+            }
+            if(levelVector[x][y] == 13){
+                spikeVector.push_back(new Spike(sf::Vector2f(x * tileSize.x,y * tileSize.y), sf::Vector2f(1,24),sf::Vector2f(tileSize.x-1,tileSize.x-24), sf::Vector2f(tileSize.x/2,tileSize.y/2)));
+                spikeVector[spikeVector.size()-1]->rotate(270);
+                
             }
         }
     }
-    
     return tileVector;
     
     
--- SourceFiles/LevelLoader.h
+++ SourceFiles/LevelLoader.h
@@ -14,6 +14,38 @@
 #include <vector>
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
-std::vector<sf::RectangleShape*> loadLevelFromString(std::string levelString,int levelWidth, int levelHeight,sf::Vector2f tileSize);
+class Spike{
+public:
+    sf::Vector2f position,leftCornerOffset,dimensions, rotationalCenter;
+    sf::FloatRect collisionBox;
+    sf::Sprite spikeSprite;
+    sf::RectangleShape rect;
+    
+    Spike(sf::Vector2f position, sf::Vector2f leftCornerOffset, sf::Vector2f dimensions, sf::Vector2f rotationalCenter){
+        this->position = position;
+        this->leftCornerOffset = leftCornerOffset;
+        this->dimensions = dimensions;
+        this->rotationalCenter = rotationalCenter;
+        collisionBox.left = position.x + leftCornerOffset.x;
+        collisionBox.top = position.y + leftCornerOffset.y;
+        collisionBox.height = dimensions.y;
+        collisionBox.width = dimensions.x;
+        spikeSprite.setPosition(position.x, position.y);
+        spikeSprite.setOrigin(rotationalCenter);
+        spikeSprite.move(rotationalCenter);
+        rect.setSize(sf::Vector2f(collisionBox.width,collisionBox.height));
+        rect.setPosition(sf::Vector2f(sf::Vector2f(collisionBox.left,collisionBox.top)));
+        rect.setOrigin(rotationalCenter.x - leftCornerOffset.x,rotationalCenter.y - leftCornerOffset.y);
+        rect.move(rotationalCenter.x - leftCornerOffset.x,rotationalCenter.y - leftCornerOffset.y);
+    }
+    void rotate(float degrees){
+        
+        rect.rotate(degrees);
+        collisionBox = rect.getGlobalBounds();
+        spikeSprite.rotate(degrees);
+        
+    }
+};
+std::vector<sf::RectangleShape*> loadLevelFromString(std::string levelString,int levelWidth, int levelHeight,sf::Vector2f tileSize, std::vector<Spike*> & spikeVector);
 
 #endif /* defined(__Platformer2__LevelLoader__) */
