044efcb35abb0ff9bd51edbf3cd973742496ab14
JamesCraster <jamescraster99@gmail.com>
2016-10-08 15:53:23 +0100

    Update Entities.h

--- SourceFiles/Entities.h
+++ SourceFiles/Entities.h
@@ -1,6 +1,6 @@
 //
 //  Entities.h
-//  Platformer2
+//  StealthPlatformer
 //
 //  Created by James Vaughan Craster on 07/10/2016.
 //  Copyright (c) 2016 James Vaughan Craster. All rights reserved.
